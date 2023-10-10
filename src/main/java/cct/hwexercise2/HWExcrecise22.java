58995fb39342b46d1464fc36c7de223090b11a01
Luis Ramirez Vega <2023169@student.cct.ie>
2023-10-10 17:17:10 +0100

    Last update Done

--- src/main/java/cct/hwexercise2/HWExcrecise22.java
+++ src/main/java/cct/hwexercise2/HWExcrecise22.java
@@ -4,6 +4,8 @@
  */
 package cct.hwexercise2;
 
+import java.util.Scanner;
+
 /**
  * 3)	Create a program that will ask the user for a number and then output a 
  * message saying whether the number is ODD or EVEN. 
@@ -19,8 +21,27 @@ public class HWExcrecise22 {
     
     public static void main(String[] args) {
     
+        Scanner myKB = new Scanner(System.in);
+        int number;
+        
+        try{
+            System.out.println("Give me one number: ");
+            number = myKB.nextInt();
+            
+            if((number %2) == 0){
+                System.out.println("This is a ODD number");
+            }
+            
+            else{
+                System.out.println("This is a EVEN number");
+            }
+        }
+            
+        catch(Exception e){
+                System.out.println("This is not a number!");
+        }
     }
-    if((number %2) == 0){
+    
 
 }
 }
