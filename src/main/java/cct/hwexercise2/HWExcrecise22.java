66e3cb05fc76721a219ae38e4bee00810a438e37
Luis Ramirez Vega <2023169@student.cct.ie>
2023-10-10 17:03:43 +0100

    First update

---
+++ src/main/java/cct/hwexercise2/HWExcrecise22.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cct.hwexercise2;
+
+/**
+ * 3)	Create a program that will ask the user for a number and then output a 
+ * message saying whether the number is ODD or EVEN. 
+ * Reminder: modulus  % in Java. A number is EVEN if number % 2 equals 0
+ */
+
+
+/**
+ *
+ * @author PC-1
+ */
+public class HWExcrecise22 {
+    
+    public static void main(String[] args) {
+    
+    }
+    if((number %2) == 0){
+
+}
+}
