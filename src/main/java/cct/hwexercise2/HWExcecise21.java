9cdee17adb887e447980499876b51dcbe9ef530e
Luis Ramirez Vega <2023169@student.cct.ie>
2023-10-10 16:38:35 +0100

    all structure done, need last part

--- src/main/java/cct/hwexercise2/HWExcecise21.java
+++ src/main/java/cct/hwexercise2/HWExcecise21.java
@@ -4,6 +4,15 @@
  */
 package cct.hwexercise2;
 
+import java.util.Scanner;
+
+/**
+ * 
+ * Create a program that will ask the user for two numbers and then calculate 
+ * the result of DIVIDING the first number by the second number (NB – this might
+ * not be an integer!).
+ */
+
 /**
  *
  * @author PC-1
@@ -12,7 +21,22 @@ public class HWExcecise21 {
     
     public static void main(String[] args) {
     
-    
+        Scanner myKB = new Scanner(System.in);
+        
+        int number1, number2;
+        
+        try{
+        
+            System.out.println("Write the first number: ");
+            number1 = myKB.nextInt();
+            System.out.println("Write the second number: ");
+            number2 = myKB.nextInt();
+            System.out.println("");
+        }
+        catch(Exception e){
+            
+            System.out.println("This is not a number! ");
+        }
     }
-    
 }
+    
